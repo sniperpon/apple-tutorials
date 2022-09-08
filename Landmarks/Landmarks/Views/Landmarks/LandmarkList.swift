c279b2afeddf16818494b5177bb8d9e6e6644358
sniperpon <11429394+sniperpon@users.noreply.github.com>
2022-09-07 19:44:46 -0500

    Finished Landmarks tutorial as far as I was interested

---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,51 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Sniperpon on 9/5/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        /*ForEach(["iPhone SE (2nd generation)", "iPhone XS Max", "iPad (9th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        .previewInterfaceOrientation(.portraitUpsideDown)*/
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
